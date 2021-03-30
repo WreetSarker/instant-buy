62776f4abcc20fa663dfd78e94593146cc639171
WreetSarker <wreet.sarker@gmail.com>
2021-03-30 22:59:59 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDaJL3tfMFtlYOfeTkURDtyXyqKZ2hfPy4",
+    authDomain: "instant-buy-wreet.firebaseapp.com",
+    projectId: "instant-buy-wreet",
+    storageBucket: "instant-buy-wreet.appspot.com",
+    messagingSenderId: "373970933854",
+    appId: "1:373970933854:web:1f9aa519ad2e1a8abdbccf"
+};
+
+export default firebaseConfig;
